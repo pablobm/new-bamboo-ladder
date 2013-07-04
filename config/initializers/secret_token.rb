6bd3c54ffb190551def365861dafb765634aa305
Pablo Brasero Moreno <pablobm@gmail.com>
2013-07-04 21:28:39 +0100

    Pristine Rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ladder::Application.config.secret_key_base = '9fd0e9d8341e5bf819df99f259f8375d9b170e6ddfc28bcc172175f703b476d1f3805392e1dc0d9414d4582caa6e1ca9f1c42d3f5c8136e7d7fa3dd37ccdb9d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ladder::Application.config.session_store :cookie_store, key: '_ladder_session'
