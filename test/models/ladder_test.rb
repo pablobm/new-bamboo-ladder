0c0048dce06364891047201c99c0950486042f07
Pablo Brasero Moreno <pablobm@gmail.com>
2013-07-08 18:28:14 +0100

    This is clearer

--- test/models/ladder_test.rb
+++ test/models/ladder_test.rb
@@ -3,51 +3,38 @@ require 'test_helper'
 class LadderTest < ActiveSupport::TestCase
 
   def resolve_result(winner, loser)
-    result = Result.create!(winner: winner, loser: loser)
-    ladder = Ladder.new(User.all)
-    ladder.resolve(result)
-    winner.reload
-    loser.reload
+    result = Result.create!(winner: users(winner), loser: users(loser))
+    @ladder.resolve(result)
   end
 
+  def assert_ladder(*expected_order)
+    assert_equal expected_order.map{|name| users(name).name }, @ladder.players.map(&:name)
+  end
+
+
+  def setup
+    @ladder = Ladder.new
+  end
+
+
   test "nothing changes if someone beats a player below them" do
-    p1 = users(:alice)
-    pos1 = p1.position
-    p2 = users(:bob)
-    pos2 = p2.position
-    resolve_result(p1, p2)
-    assert_equal pos1, p1.position
-    assert_equal pos2, p2.position
+    resolve_result(:alice, :bob)
+    assert_ladder(:alice, :bob, :carol, :dan, :erin)
   end
 
   test "beat anyone above you and you move up one" do
-    p1 = users(:erin)
-    pos1 = p1.position
-    p2 = users(:alice)
-    pos2 = p2.position
-    resolve_result(p1, p2)
-    assert_equal pos1+1, p1.position
-    assert_equal pos2-1, p2.position
+    resolve_result(:erin, :alice)
+    assert_ladder(:bob, :alice, :carol, :erin, :dan)
   end
 
   test "if a person beats the person above them, they change places" do
-    p1 = users(:bob)
-    pos1 = p1.position
-    p2 = users(:alice)
-    pos2 = p2.position
-    resolve_result(p1, p2)
-    assert_equal pos2, p1.position
-    assert_equal pos1, p2.position
+    resolve_result(:bob, :alice)
+    assert_ladder(:bob, :alice, :carol, :dan, :erin)
   end
 
   test "if a person beats 2 places above them, they change places" do
-    p1 = users(:carol)
-    pos1 = p1.position
-    p2 = users(:alice)
-    pos2 = p2.position
-    resolve_result(p1, p2)
-    assert_equal pos2, p1.position
-    assert_equal pos1, p2.position
+    resolve_result(:carol, :alice)
+    assert_ladder(:carol, :bob, :alice, :dan, :erin)
   end
 
 end
