afa7cb1456a065743fa020da7c9284e9c0e43127
Pablo Brasero <pablobm@gmail.com>
2013-07-09 00:19:06 +0100

    Dev auth for my offline development needs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+require 'dev_auth'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
 end
