0d72eb9eeb4ea68f36ac668b44610fd251c0263e
Pablo Brasero Moreno <pablobm@gmail.com>
2013-07-08 18:28:29 +0100

    WIP

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -1,7 +1,15 @@
 class Ladder
-  def initialize(users)
+  def resolve(result)
+    positions = User.all.select(:id, :position).order('position ASC').map{|u| [u.id, u.position] }
+    winner_index = positions.index{|e| e.first == result.winner_id}
+    loser_index = positions.index{|e| e.first == result.loser_id}
+    if winner_index > loser_index
+      # Nothing to do
+    elsif loser_index - winner_index
+    end
   end
 
-  def resolve(result)
+  def players
+    User.where('position NOT NULL').order('position ASC')
   end
 end
