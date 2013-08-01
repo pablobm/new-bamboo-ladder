19e0ec6a06a02350886dc51db7c1b92b709d4def
Pablo Brasero <pablobm@gmail.com>
2013-08-01 20:14:34 +0100

    Desired end result

---
+++ test/integration/undo_result_test.rb
@@ -0,0 +1,16 @@
+require 'integration_test_helper'
+
+class UndoResultTest < CapybaraTestCase
+
+  test "undoing an incorrect result" do
+    login
+
+    submit_result("Erin", "Alice")
+    assert_ladder ['Bob', 'Alice', 'Carol', 'Erin', 'Dan']
+    within '#flash' do
+      click_link "undo"
+    end
+    assert_ladder ['Alice', 'Bob', 'Carol', 'Dan', 'Erin']
+  end
+
+end
