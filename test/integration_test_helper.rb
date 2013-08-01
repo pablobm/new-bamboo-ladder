cfbd7324ab96a2bd9721a50477f818154cc30b0c
Pablo Brasero <pablobm@gmail.com>
2013-08-01 20:45:53 +0100

    Refactor for reuse

---
+++ test/integration_test_helper.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+def login
+  visit root_path
+  click_link "Log in"
+  fill_in "Email", with: User.first.email
+  click_button "Log in"
+end
+
+def submit_result(winner, loser)
+  select winner, from: 'result[winner_id]'
+  select loser, from: 'result[loser_id]'
+  within '.result-form' do
+    find('[type=submit]').click
+  end
+end
+
+def assert_ladder(expected_names)
+  assert_equal expected_names, ladder_names
+end
+
+def ladder_names
+  all('.ladder-name').map(&:text)
+end
