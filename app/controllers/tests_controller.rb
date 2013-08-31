73f7519917c1a7a161803ac9b2af61510021ed22
Pablo Brasero <pablobm@gmail.com>
2013-08-31 09:27:00 +0200

    Simple flashes for simple messages

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,6 +2,7 @@ class TestsController < ApplicationController
 
   def show
     flash.now[:display] = {result: {result_id: Result.last.id}}
+    flash.now[:notice] = "This is a simple notice"
   end
 
 
--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -3,17 +3,11 @@ module FlashHelper
   def render_flash
     return unless flash.key?(:display)
 
-    messages = flash[:display].collect do |msg, opts|
-      begin
-        flash_class = (msg.to_s.camelize + "Flash").constantize
-        f = flash_class.new(opts)
-        render f.template, f.locals
-      rescue User
-        raise "I don't know what to do with #{msg.inspect}"
-      end
-    end
+    messages =
+      flash_display_messages +
+      flash_simple_messages
 
-    content_tag(:div, messages.join.html_safe, id: 'flash')
+    content_tag(:div, messages.html_safe, id: 'flash')
   end
 
   def player(name)
@@ -22,6 +16,27 @@ module FlashHelper
 
 
   private
+
+  def flash_display_messages
+    return unless flash.key?(:display)
+
+    flash[:display].collect do |msg, opts|
+      begin
+        flash_class = (msg.to_s.camelize + "Flash").constantize
+        f = flash_class.new(opts)
+        render f.template, f.locals
+      rescue User
+        raise "I don't know what to do with #{msg.inspect}"
+      end
+    end.join
+  end
+
+  def flash_simple_messages
+    [:notice, :alert].map do |k|
+      next unless text = flash[k]
+      render 'flashes/simple', type: k, text: text
+    end.join
+  end
 
   def flash_result(opts)
     result_id = opts.fetch(:result_id)
