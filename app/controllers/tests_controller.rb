81109e142c062fa91598bdc6d9385371e67064b4
Pablo Brasero <pablobm@gmail.com>
2013-09-03 23:55:57 +0200

    Gold, silver and bronze

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,11 +3,12 @@ class TestsController < ApplicationController
   def show
     first = User.in_order.first
     second = User.in_order.second
+    third = User.in_order.third
     last = User.in_order.last
 
     display_message_now(:result, result: Result.new(winner: first, loser: second, previous_state: User.state))
     display_message_now(:result, result: Result.new(winner: second, loser: first, previous_state: User.state))
-    display_message_now(:result, result: Result.new(winner: second, loser: last, previous_state: User.state))
+    display_message_now(:result, result: Result.new(winner: third, loser: last, previous_state: User.state))
     display_message_now(:result, result: Result.new(winner: last, loser: second, previous_state: User.state))
 
     flash.now[:notice] = "This is a simple notice"
