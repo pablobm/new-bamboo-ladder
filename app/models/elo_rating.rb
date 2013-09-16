095e0b1dda035ec4127c6e78ab0d59c7a590ef68
Pablo Brasero Moreno <pablobm@gmail.com>
2013-09-16 23:41:46 +0100

    Decay once a week

--- app/models/elo_rating.rb
+++ app/models/elo_rating.rb
@@ -1,16 +1,10 @@
 class EloRating
   include Singleton
 
-  def decay
-    active_ids = Result.where('created_at > ?', inaction_threshold).map{|r| [r.winner_id, r.loser_id] }.flatten.uniq
-    inactive_ids = Player.where('id NOT IN (?)', active_ids).map(&:id)
-    if Result.where('created_at < ?', inaction_threshold).exists? && inactive_ids.any?
-      Player.where('id IN (?)', active_ids).update_all("elo_rating = elo_rating + #{inactive_ids.count.to_i}")
-      Player.where('id IN (?)', inactive_ids).update_all("elo_rating = elo_rating - #{active_ids.count.to_i}")
-    end
-  end
-
   def resolve(result)
+    if first_result_of_week?(result)
+      decay
+    end
     update_players(result.winner, result.loser)
     top_up_pool
   end
@@ -19,6 +13,13 @@ class EloRating
   private
 
   INACTION_LIMIT = 4.weeks
+
+  def first_result_of_week?(result)
+    previous = Result.in_order.where('id < ?', result.id).last or return true
+    week_for_previous = previous.created_at.to_date.cweek
+    week_for_current = result.created_at.to_date.cweek
+    week_for_previous != week_for_current
+  end
 
   def inaction_threshold
     Time.now - INACTION_LIMIT
@@ -35,6 +36,16 @@ class EloRating
       winner.save!
     end
   end
+
+  def decay
+    active_ids = Result.where('created_at > ?', inaction_threshold).map{|r| [r.winner_id, r.loser_id] }.flatten.uniq
+    inactive_ids = Player.where('id NOT IN (?)', active_ids).map(&:id)
+    if Result.where('created_at < ?', inaction_threshold).exists? && inactive_ids.any?
+      Player.where('id IN (?)', active_ids).update_all("elo_rating = elo_rating + #{inactive_ids.count.to_i}")
+      Player.where('id IN (?)', inactive_ids).update_all("elo_rating = elo_rating - #{active_ids.count.to_i}")
+    end
+  end
+
 
   def top_up_pool
     while Player.count*initial_rating - Player.sum(:elo_rating) > Player.count
