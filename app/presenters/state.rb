7cf614724abf471f51aaaeba11ae65f04a74dac5
Iain Beeston <iain.beeston@gmail.com>
2014-07-02 11:36:49 +0100

    Tidied up how we calculate (legacy) positions from state

--- app/presenters/state.rb
+++ app/presenters/state.rb
@@ -13,44 +13,36 @@ class State
   end
 
   def self.load(dump)
-    if dump.key?('positions')
-      load_with_positions(dump)
-    else
-      load_without_positions(dump)
-    end
+    # calculate positions if not present (older states don't have positions)
+    dump['positions'] = positions_from_elos(dump['elo_ratings']) if !dump.key?('positions')
+
+    elos = dump.fetch('elo_ratings', {})
+    positions = dump.fetch('positions', {})
+    non_positioned, positioned = positions.partition { |_, pos| pos.nil? }
+    players = (positioned + non_positioned).map { |uid, position| Player.new(uid, elos[uid], position) }
+    new(players)
   end
 
-
   private
+
+  def self.positions_from_elos(elos)
+    positions = {}
+    position = 1
+    previous_elo = nil
+    non_rated, rated = elos.partition{|_, elo| elo.nil? }
+    rated.sort_by{|_, elo| -elo }.each_with_index.map do |(uid, elo), i|
+      position = i+1 if previous_elo != elo
+      previous_elo = elo
+      positions[uid] = position
+    end
+    non_rated.each { |uid, _| positions[uid] = nil }
+    positions
+  end
 
   class Player < Struct.new(:id, :elo_rating, :position); end
 
   def initialize(players)
     @players = players
-  end
-
-  class << self
-    def load_with_positions(dump)
-      elos = dump['elo_ratings'] || {}
-      positions = dump['positions'] || {}
-      non_positioned, positioned = positions.partition { |_, pos| pos.nil? }
-      players = (positioned + non_positioned).map { |uid, position| Player.new(uid, elos[uid], position) }
-      new(players)
-    end
-
-    def load_without_positions(dump)
-      elos = dump['elo_ratings'] || {}
-      position = 1
-      previous_elo = nil
-      non_rated, rated = elos.partition{|_, elo| elo.nil? }
-      rated_players = rated.sort_by{|_, elo| -elo }.each_with_index.map do |(uid, elo), i|
-        position = i+1 if previous_elo != elo
-        previous_elo = elo
-        Player.new(uid, elo, position)
-      end
-      non_rated_players = non_rated.map{|id, _| Player.new(id)}
-      new(rated_players + non_rated_players)
-    end
   end
 
 end
